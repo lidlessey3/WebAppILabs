f365445171e5d40b05ab07180c371a654859ddf8
lidlessey3 <leonardo.pavarino@gmail.com>
2023-05-11 18:30:37 +0200

    added lateral bar functionality

--- reactLabs/src/components/lateralBar.jsx
+++ reactLabs/src/components/lateralBar.jsx
@@ -1,3 +1,20 @@
-function LateralBar(props) { }
+function LateralBar(props) {
+    return (
+        <div className="list-group list-group-flush">
+                {Object.values(props.filters).map((filter) => <Filter data={filter} key={filter.id} updateFunc={props.updateFunc} selected={props.selected === filter.id}></Filter>)}
+        </div>
+    );
+}
+
+function Filter(props) {
+    let classes = "list-group-item list-group-item-action";
+    if (props.selected)
+        classes += " active";
+    return (<>
+        <a href="#" onClick={() => props.updateFunc(props.data.id)} className={classes}>
+            <p>{props.data.label}</p>
+        </a>
+    </>);
+}
 
 export default LateralBar;
