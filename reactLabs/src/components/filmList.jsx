11d9ddfecd2aefecc3fbe3c1254f9c4007ddbc9e
lidlessey3 <leonardo.pavarino@gmail.com>
2023-05-13 18:35:33 +0200

    Fixed formatting

--- reactLabs/src/components/filmList.jsx
+++ reactLabs/src/components/filmList.jsx
@@ -5,46 +5,46 @@ import NewFilmForm from './newFilmForm';
 
 let FilmList = (props) => {
   const [addFormVisible, setAddFormVisible] = useState(false);
-   
+
   return (
-      <>
-        <h1>{props.filter.label}</h1>
-        <Table striped>
-          <tbody>
-            {props.films.filter(props.filter.filterFunction).map((film) => <FilmRow filmData={film} key={film.id} />)}
-          </tbody>
+    <>
+      <h1>{props.filter.label}</h1>
+      <Table striped>
+        <tbody>
+          {props.films.filter(props.filter.filterFunction).map((film) => <FilmRow filmData={film} key={film.id} />)}
+        </tbody>
       </Table>
-      
-        <div className='d-grid gap-2'>
-          <Button variant={addFormVisible? 'outline-danger' : 'outline-primary'} size="lg" onClick={() => setAddFormVisible(!addFormVisible)}> 
-            { addFormVisible? <i key="minusButton" className='bi bi-x-circle' /> : <i key="plusButton" className='bi bi-plus' />}
-          </Button>
-        </div>
-      </>
+
+      <div className='d-grid gap-2'>
+        <Button variant={addFormVisible ? 'outline-danger' : 'outline-primary'} size="lg" onClick={() => setAddFormVisible(!addFormVisible)}>
+          {addFormVisible ? <i key="minusButton" className='bi bi-x-circle' /> : <i key="plusButton" className='bi bi-plus' />}
+        </Button>
+      </div>
+    </>
   );
 }
 
 function FilmRow(props) {
-   
+
   const formatWatchDate = (dayJsDate, format) => {
     return dayJsDate ? dayJsDate.format(format) : '';
   }
 
-  return(
+  return (
     <tr>
       <td>
-         <p className={props.filmData.favorite ? "favorite" : ""} >
+        <p className={props.filmData.favorite ? "favorite" : ""} >
           {props.filmData.title}
         </p>
       </td>
       <td>
-        <Form.Check type="checkbox" label="Favorite" defaultChecked={props.filmData.favorite ? true : false}/>
+        <Form.Check type="checkbox" label="Favorite" defaultChecked={props.filmData.favorite ? true : false} />
       </td>
       <td>
         <small>{formatWatchDate(props.filmData.date, 'YYYY/MM/DD')}</small>
       </td>
       <td>
-        <Rating rating={props.filmData.rating} maxStars={5}/>
+        <Rating rating={props.filmData.rating} maxStars={5} />
       </td>
     </tr>
   );
